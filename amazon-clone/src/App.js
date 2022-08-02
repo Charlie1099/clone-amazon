787aefa5da1abaa902c178f08c0de7b7992ef1a0
charlie1099 <cdcharlesworth@gmail.com>
2022-08-01 22:38:10 -0400

    started home page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from "./Home"
+
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
     <div className="App">
      <Header />
      
-     {/*Home*/}
+     <Home />
      {/*Footer*/}
     </div>
   );
