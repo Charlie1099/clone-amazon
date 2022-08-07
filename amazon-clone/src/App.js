d0ca5796d8cadb7bdf46735370c2129eb5472e7d
charlie1099 <cdcharlesworth@gmail.com>
2022-08-06 21:44:44 -0400

    added checkout

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Home from "./Home"
-
+import Checkout from "./Checkout"
+import { BrowserRouter as Router, Switch, Route }
+  from "react-router-dom"
 
 function App() {
   return (
     //BEM
-    <div className="App">
-     <Header />
-     
-     <Home />
-     {/*Footer*/}
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path='/checkout'>
+            
+            <Checkout />
+          </Route>
+          <Route path='/'>
+            
+            <Home />
+          </Route>
+          {/*Footer*/}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
+            <Link to="/">
             <img className='header__logo' src='https://pngimg.com/uploads/amazon/amazon_PNG25.png' />
+            </Link>
+            
 
             <div className='header__search'>
                 <input className='header__searchInput' type='text' />
@@ -29,10 +33,12 @@ function Header() {
                     <span className='header__optionLineTwo'>Prime</span>
                 </div>
 
+                <Link to="/checkout">
                 <div className='header__optionBasket'>
                     <ShoppingCartIcon />
                     <span className='header__optionLineTwo header__basketCount'>0</span>
                 </div>
+                </Link>
 
             </div>
 
